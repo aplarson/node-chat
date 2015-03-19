ae09625fdc93cdaab35bb2c09b0aee30cc8f21d4
aplarson <andrew.p.larson@gmail.com>
2015-03-19 10:25:17 -0700

    Show nicknames on client side

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -8,12 +8,14 @@
     this.chat = new ChatApp.Chat(socket);
     this.$input = $("#chat-input");
     this.$display = $("#chat-display");
+    this.$nicknames = $("#nickname-display");
     this.$input.on("submit", this.submit.bind(this));
     socket.on("message", this.receive.bind(this));
+    socket.on("nicknameUpdate", this.updateNicknames.bind(this));
   };
 
   ChatUI.prototype.receive = function (broadcast) {
-    var $messageEl = $("<p>").text(broadcast.message.message);
+    var $messageEl = $("<p>").text(broadcast.message.message + " by " + broadcast.author);
     this.$display.append($messageEl);
   };
 
@@ -22,5 +24,12 @@
     var textarea = this.$input.find("#chat_content")
     this.chat.sendMessage(textarea.val());
     textarea.val("");
+  };
+
+  ChatUI.prototype.updateNicknames = function (message) {
+    this.$nicknames.empty();
+    for (var id in message.nicknames) {
+      this.$nicknames.append($("<p>").text(message.nicknames[id]));
+    }
   };
 })();
